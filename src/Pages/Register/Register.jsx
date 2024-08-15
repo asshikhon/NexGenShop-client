f96e179fed11639eaef6a07dea006c64039d6124
asshikhon <asshikhon@gmail.com>
2024-08-15 14:53:30 +0600

    create initial file

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,11 @@
+
+
+const Register = () => {
+    return (
+        <div>
+            <h3 className="text-3xl">This is Register page</h3>
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h3 className="text-3xl">This is Navbar</h3>
+        </div>
+    );
+};
+
+export default Navbar;
